06af3aea662fb3aeac61dc454b54329aaf77610e
hexagon431 <hexagon431@gmail.com>
2017-11-16 09:22:34 -0700

    Initial commit with code snippet from class to get started

---
+++ scripts/golf.js
@@ -0,0 +1,11 @@
+var closeCourses;
+var local_obj = {latitude: 40.4426135, longitude: -111.8631116, radius: 100};
+
+function loadMe(){
+    $.post("https://golf-courses-api.herokuapp.com/courses", local_obj, function(data, status){
+        closeCourses = JSON.parse(data);
+        for (var p in closeCourses.courses){
+            console.log(closeCourses.courses[p].name);
+        }
+    });
+}
